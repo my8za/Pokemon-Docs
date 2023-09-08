43738f866f8306cb01d0d88d8594c46a31b63ad8
my8za <emilie15@naver.com>
2023-09-08 22:33:16 +0900

    firebase 배포

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom'
 // components
 import Header from '../layouts/Header'
 import Search from '../components/search_bar/Search'
-import DetailedSearch from '../components/search_bar/DetailedSearch'
 import Footer from '../layouts/Footer'
 
 // style
@@ -15,7 +14,6 @@ const index = () => {
     <div>
       <Header />
       <Search />
-      {/* <DetailedSearch />  상세검색 미완 */}
       <Outlet />
       <Footer />
     </div>
